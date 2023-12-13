4ef168a657d9d89a0b7a709e9ec3ddb18f08e353
Kemal Anıl Güvenç <82261358+Anilgvnc@users.noreply.github.com>
2023-12-13 16:02:22 +0300

    last-dev update
    js documents updated for homework.

---
+++ js/router.js
@@ -0,0 +1,19 @@
+function toIndex() {
+    window.location.href = "../html/index.html";
+}
+
+function toProfile() {
+    window.location.href = "../html/profile.html";
+}
+
+function toLogin() {
+    window.location.href = "../html/login.html";
+}
+
+function toRegister() {
+    window.location.href = "../html/register.html";
+}
+
+function toSettings() {
+    window.location.href = "../html/settings.html";
+}
